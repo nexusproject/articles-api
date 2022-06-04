a09820370c6e42e7c73c883763e9569f5ac97b26
Dmitry Sergeev <sergeev@reg.ru>
2022-06-04 20:55:52 +0300

    DAL Repository test for method delete

---
+++ tests/dal/repository/test_repository_delete.py
@@ -0,0 +1,32 @@
+"""Tests for DAL Repository delete.
+
+Author: Dmitry Sergeev <realnexusway@gmail.com>
+"""
+
+from articles.dal.repository import Repository
+
+import pytest
+
+import sqlalchemy
+
+
+@pytest.mark.asyncio()
+async def test_repository_delete(session: sqlalchemy.orm.Session) -> None:
+    """Testing DAL Repository method delete.
+
+    :param session: SQLAlchemy session object.
+    """
+    async with session, session.begin():
+        res = await session.execute(
+            "INSERT INTO articles (topic, text) VALUES ('some topic', 'some text')"
+        )
+
+    await Repository().delete(res.lastrowid)
+
+    async with session:
+        found = await session.execute(
+            "SELECT count(1) FROM articles WHERE article_id=:id",
+            {"id": res.lastrowid},
+        )
+
+    assert found.scalars().one() == 0
