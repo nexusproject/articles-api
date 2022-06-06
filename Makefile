7ced1dcca50518704896851ad79ff0102b83b326
Dmitry Sergeev <sergeev@reg.ru>
2022-06-06 15:05:03 +0300

    Docker compose files, Makefile, etc

--- Makefile
+++ Makefile
@@ -1,4 +1,25 @@
-run:
-	poetry run uvicorn articles.api:app --host 0.0.0.0 --port 8000 --reload
+TAG = ghcr.io/nexusproject/xx/articles-api:latest
+
+build:
+	docker build --tag $(TAG) .
+
+push: build
+    docker push $(TAG)
+
+test: export DATABASE_URL=$(TEST_DATABASE_URL)
 test:
+	dbmate up
 	env PYTHONPATH=. poetry run pytest --flake8
+	dbmate drop
+
+test_docker:
+	docker compose -f docker-compose.testing.yml up --abort-on-container-exit --force-recreate
+
+migrate:
+	dbmate up
+
+run: migrate
+	poetry run uvicorn articles.api:app --host 0.0.0.0 --port 8000 --reload
+
+run_docker:
+	docker compose up --abort-on-container-exit
--- articles/dal/db.py
+++ articles/dal/db.py
@@ -3,11 +3,34 @@
 Author: Dmitry Sergeev <realnexusway@gmail.com>
 """
 
+import os
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from .exception import DALException
+
+
+def get_database_url() -> str:
+    """Making database url.
+
+    Function making SQLAlchemy database url based on
+    DATABASE_URL enviroment variable.
+
+    :returns: SQLAlchemy database url
+    :raises DALException: If enviroment var DATABASE_URL is not setted
+    """
+    env_url = os.getenv('DATABASE_URL')
+
+    if not env_url:
+        raise DALException('No DATABASE_URL enviroment variable is setted.')
+
+    _, creds = env_url.split('://')
+
+    return "mysql+aiomysql" + "://" + creds
 
 
 engine = create_async_engine(
-    "mysql+aiomysql://root:@localhost/articles",
+    get_database_url(),
     echo=False,
     pool_size=10,
     max_overflow=5,
